52d038a0e238d139cb09a170c9bb0d48f5e1f516
Dylan Roberts <dylangardnerroberts@gmail.com>
2021-04-11 11:49:03 -0300

    master - Displaying search results and status

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -6,6 +6,7 @@ const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('')
   const { searchRepositories } = useActions();
   const { data, error, loading } = useTypedSelector((state) => state.repositories);  
+  
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
@@ -18,6 +19,10 @@ const RepositoriesList: React.FC = () => {
         <input value={term} onChange={e => setTerm(e.target.value)}/>
         <button>Search</button>
         </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && 
+        data.map((name) => <div key={name}>{name}</div>)}
     </div>
   );
 };
